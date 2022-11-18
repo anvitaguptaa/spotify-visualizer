832db05bc941f0f5a7737be2c3e0160acd9acaf5
Anvita Gupta <anvitagupta@Anvitas-MacBook-Pro.local>
2022-11-18 00:34:55 -0500

    Large cover implemented design and mobile margins

--- app.py
+++ app.py
@@ -1,7 +1,61 @@
+import spotipy
+from dotenv import load_dotenv
+import os
+import sys
+import spotipy
+from spotipy import oauth2
+import spotipy.util as utils
 from flask import Flask, render_template
 
+load_dotenv('credentials.env')
 app = Flask(__name__)
+
+CLIENT_SECRET = os.getenv('SPOTIPY_CLIENT_SECRET')
+CLIENT_ID = os.getenv('SPOTIPY_CLIENT_ID')
+REDIRECT_URI = os.getenv('SPOTIPY_REDIRECT_URI')
+USERNAME = 'av_pr'
+SCOPE = '''user-top-read user-read-playback-state user-read-recently-played 
+           user-follow-read playlist-read-private'''
+URL = 'https://api.spotify.com/v1/'
+
+
+def oauth_token():
+    token = utils.prompt_for_user_token(USERNAME, SCOPE, CLIENT_ID,
+                                       CLIENT_SECRET, REDIRECT_URI)
+
+    return token
+
+
+def make_headers(token):
+    headers = {'Authorization': 'Bearer ' + token}
+
+    return headers
+
+class Client:
+    def __init__(self):
+        self.token = oauth_token()
+        self.headers = make_headers(self.token)
+        self.client = spotipy.Spotify(auth=self.token)
+
+    def get_top_tracks(self):
+        top_tracks = self.client.current_user_top_tracks(time_range='long_term')
+        print(top_tracks)
+        tracks_arr = []
+
+        for track in top_tracks['items']:
+            tracks_arr.append(track['name'].upper() + ' - ' + track['artists'][0]['name'].upper())
+            
+        # print(tracks_arr)
+        return tracks_arr
+
+
+if __name__ == "__main__":
+    client = Client()
+    top_tracks = client.get_top_tracks()
+
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    client = Client()
+    top_tracks = client.get_top_tracks()
+    return render_template('index.html', top_tracks=top_tracks)
