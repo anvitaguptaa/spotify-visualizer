042cc9e67cd27702408978ecc907190414caf45a
Anvita Gupta <anvitagupta@Anvitas-MacBook-Pro.local>
2022-11-19 18:29:56 -0500

    Added js for vinyl

--- static/index.js
+++ static/index.js
@@ -1,12 +1,27 @@
+const small_cover = document.querySelector(".cover");
+const vinyl = document.querySelector(".vinyl");
+let small_cover_clicked = false;
+
+small_cover.addEventListener("click", () => {
+    if(!small_cover_clicked) {
+        vinyl.style.transform = "translate(450px, 0) rotate(0deg)"
+    }   else {
+        vinyl.style.transform = "translate(50px, 0) rotate(-180deg)"
+    }
+    small_cover_clicked = !small_cover_clicked;
+});
+
+
 const cover = document.querySelector(".large-cover");
 const coverContent = document.querySelector(".large-cover-inner");
-let flipped = false;
+let cover_flipped = false;
 
 cover.addEventListener("click", () => {
-    if(!flipped) {
+    if(!cover_flipped) {
         coverContent.style.transform = "rotateY(180deg)"
     }   else {
         coverContent.style.transform = "rotateY(0deg)"
     }
-    flipped = !flipped;
+    cover_flipped = !cover_flipped;
 });
+
